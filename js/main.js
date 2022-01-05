550a7d0f1371cc5e936a9d9faeb6496c4ffcfae4
marcosarango <sar21005@byui.edu>
2022-01-05 18:12:56 -0500

    update week1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const links = [
-    { label: "Week 1 notes", url: "week1/index.html" },
+    { label: "Week 1 notes", url: "/week1/index.html" },
   ];
   
-  let list = document.getElementById("list");
+let list = document.getElementById("list");
   
+for (let i = 0; i < links.length; i++) {
+  let li = document.createElement("li");
+  let label = links[i].label;
+  let url = links[i].url;
+  li.innerHTML = "<a href='" + url + "'>" + label + "</a>";
+  list.appendChild(li);
+}
