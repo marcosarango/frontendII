8652b47c801ec4a0d94a4285b3151ed9fef564b6
marcosarango <sar21005@byui.edu>
2022-01-07 19:01:49 -0500

    update week1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const links = [
-  { label: "Week 1 notes", url: "/week1/index.html" }
+  { label: "Week 1 notes", url: "/week1/index.html", notes: "Porfolio" },
   ]
   
 let list = document.getElementById("list");
   
 for (let i = 0; i < links.length; i++) {
+  // in this part we create an element <li> <a></a> </li>
   let li = document.createElement("li");
-  let label = links[i].label;
-  let url = links[i].url;
-  li.innerHTML = "<a href='" + url + "'>" + label + "</a>";
-  list.appendChild(li);
-}
+  let a  = document.createElement('a');
+
+  a.setAttribute('href', links[i].url); //set a link/url for Week[i]
+  a.textContent = links[i].label + " : " + links[i].notes;  // set a Label of Week[i]
+
+  li.appendChild(a); // put the a inside the li
+  list.appendChild(li); // put the li inside the #list
+};
+
+  console.log("hello world!")
